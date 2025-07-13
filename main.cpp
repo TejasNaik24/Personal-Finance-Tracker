c221082a3b0773e58cf8d740d5d0378ad8350b1e
tejasnaik <naik.tejas11@gmail.com>
2025-07-13 02:03:30 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int option;
+    cout << "Welcome to your Personal Finance Tracker" << endl;
+    cout << "1. Add Income" << endl;
+    cout << "2. Add Expense" << endl;
+    cout << "3. View Report" << endl;
+    cout << "4. Export file " << endl;
+    cout << "Enter a number to choose an option: ";
+    cin >> option;
+
+    return 0;
+}
