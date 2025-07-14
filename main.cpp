ecc29a453909d35be7a02ae1c27cc3905a5d6261
tejasnaik <naik.tejas11@gmail.com>
2025-07-13 20:26:51 -0400

    added reset file option

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main() {
     cout << "1. Add Income" << endl;
     cout << "2. Add Expense" << endl;
     cout << "3. View Report" << endl;
-    cout << "4. Export file " << endl;
+    cout << "4. Export file" << endl;
+    cout << "Reset file" << endl;
     cout << "Enter a number to choose an option: ";
     cin >> option;
 
