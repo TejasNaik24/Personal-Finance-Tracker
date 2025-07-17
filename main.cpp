d2f20e8dfcd268797df00c3c68a7b11a6ffb005d
tejasnaik <naik.tejas11@gmail.com>
2025-07-16 23:56:04 -0400

    added addexpense

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@
 using namespace std;
 
 class TrackerMain {
-    public:
-        int option;
-        void run() {
-            cout << "1. Add Income" << endl;
-            cout << "2. Add Expense" << endl;
-            cout << "3. View Report" << endl;
-            cout << "4. Export file" << endl;
-            cout << "Reset file" << endl;
-            cout << "Enter a number to choose an option: ";
+public:
+    int option;
+    void run() {
+        cout << "1. Add Income" << endl;
+        cout << "2. Add Expense" << endl;
+        cout << "3. View Report" << endl;
+        cout << "4. Export file" << endl;
+        cout << "Reset file" << endl;
+        cout << "Enter a number to choose an option: ";
+        cin >> option;
+        while (option != 1) {
+            cout << "invalid option please enter a valid option" << endl;
             cin >> option;
-        };
+        }
+        if (option == 1) {
+            cout << "cool";
+        }
+    }
+    void addExpense() {};
+    
 };
 
-int main() {
+
+int main() { 
     cout << "Welcome to your Personal Finance Tracker" << endl;
 
     TrackerMain main;
