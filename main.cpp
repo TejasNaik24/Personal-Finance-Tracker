db524199bd3bcff0472a50641b283c4e00e07e02
tejasnaik <naik.tejas11@gmail.com>
2025-07-17 14:06:59 -0400

    added new classes

--- main.cpp
+++ main.cpp
@@ -11,15 +11,23 @@ public:
         cout << "2. Add Expense" << endl;
         cout << "3. View Report" << endl;
         cout << "4. Export file" << endl;
-        cout << "Reset file" << endl;
+        cout << "5. Reset file" << endl;
         cout << "Enter a number to choose an option: ";
         cin >> option;
-        while (option != 1) {
-            cout << "invalid option please enter a valid option";
+        while (option < 1 || option > 5) {
+            cout << "invalid option please enter a valid option ";
             cin >> option;
         }
         if (option == 1) {
-            cout << "cool";
+            
+        } else if (option == 2) {
+
+        } else if (option == 3) {
+
+        } else if (option == 4) {
+        
+        } else {
+
         }
     }
     void addExpense() {};
