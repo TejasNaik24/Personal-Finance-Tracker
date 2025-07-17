64ba2dd9a6df3e5de59a363c2bbe5dac5a748709
tejasnaik <naik.tejas11@gmail.com>
2025-07-16 23:44:34 -0400

    added classes

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 #include <string>
 using namespace std;
 
+class TrackerMain {
+    public:
+        int option;
+        void run() {
+            cout << "1. Add Income" << endl;
+            cout << "2. Add Expense" << endl;
+            cout << "3. View Report" << endl;
+            cout << "4. Export file" << endl;
+            cout << "Reset file" << endl;
+            cout << "Enter a number to choose an option: ";
+            cin >> option;
+        };
+};
+
 int main() {
-    int option;
-    
     cout << "Welcome to your Personal Finance Tracker" << endl;
-    cout << "1. Add Income" << endl;
-    cout << "2. Add Expense" << endl;
-    cout << "3. View Report" << endl;
-    cout << "4. Export file" << endl;
-    cout << "Reset file" << endl;
-    cout << "Enter a number to choose an option: ";
-    cin >> option;
+
+    TrackerMain main;
+    main.run();
 
     return 0;
 }
