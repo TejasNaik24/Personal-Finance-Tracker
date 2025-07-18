040f4fab945a7e82d560f2e60657a3e28f7be95c
tejasnaik <naik.tejas11@gmail.com>
2025-07-18 01:53:21 -0400

    added new class

--- main.cpp
+++ main.cpp
@@ -3,57 +3,53 @@
 #include <string>
 using namespace std;
 
-class TrackerMain {
+class TrackerAdd {
 public:
-    int option;
-    void run() {
-        cout << "1. Add Income" << endl;
-        cout << "2. Add Expense" << endl;
-        cout << "3. View Report" << endl;
-        cout << "4. Export file" << endl;
-        cout << "5. Reset file" << endl;
-        cout << "Enter a number to choose an option: ";
-        cin >> option;
-        while (option < 1 || option > 5) {
-            cout << "invalid option please enter a valid option ";
-            cin >> option;
-        }
-        if (option == 1) {
-            addIncome();
-        } else if (option == 2) {
-            addExpense();
-        } else if (option == 3) {
-            viewReport();
-        } else if (option == 4) {
-            exportFile();
-        } else {
-            restFile();
-        }
-    }
-    void addIncome() {
-
-    };
-    void addExpense() {
-
-    };
-    void viewReport() {
-
-    }
-    void exportFile() {
-
-    };
-    void restFile() {
-
-    };
+    void addIncome() {};
+    void addExpense() {};
     
 };
 
+class TrackerSettings {
+    public:
+        void viewReport() {}
+        void exportFile() {};
+        void resetFile() {};
+
+};
+
+void showMenu(TrackerAdd& add, TrackerSettings& settings) {
+    int option;
+    cout << "1. Add Income" << endl;
+    cout << "2. Add Expense" << endl;
+    cout << "3. View Report" << endl;
+    cout << "4. Export file" << endl;
+    cout << "5. Reset file" << endl;
+    cout << "Enter a number to choose an option: ";
+    cin >> option;
+    while (option < 1 || option > 5) {
+        cout << "invalid option please enter a valid option ";
+        cin >> option;
+    }
+    if (option == 1) {
+        add.addIncome();
+    } else if (option == 2) {
+        add.addExpense();
+    } else if (option == 3) {
+        settings.viewReport();
+    } else if (option == 4) {
+        settings.exportFile();
+    } else {
+        settings.resetFile();
+    }
+}
+
 
 int main() { 
+    TrackerAdd add;
+    TrackerSettings settings;
     cout << "Welcome to your Personal Finance Tracker" << endl;
-
-    TrackerMain main;
-    main.run();
+    showMenu(add, settings);
 
     return 0;
 }
